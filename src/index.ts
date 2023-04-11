52abf7dfab6c9204ecd7f0cc2ebd74a847f99d06
teo-garcia <juan.garcia@gorillalogic.com>
2023-04-11 17:07:01 -0500

    feat: #0 add basic auth

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import swagger from '@fastify/swagger'
 import swaggerUI from '@fastify/swagger-ui'
 import sensible from '@fastify/sensible'
 import postgres from '@fastify/postgres'
-// import fastifyAuth from '@fastify/auth'
-// import fastifyJwt from '@fastify/jwt'
+import fastifyAuth from '@fastify/auth'
+
+import fastifyBasicAuth from '@fastify/basic-auth'
 
 import { SwaggerConfig } from '@config/swagger.config'
 import { FastifyConfig } from '@config/fastify.config'
 import { PostgresConfig } from '@config/postgres.config'
-// import { JwtConfig } from '@config/jwt.config'
 
 import { TodosRouter } from '@routers/todos.router'
 import { HealthRouter } from '@routers/health.router'
@@ -18,16 +18,40 @@ import { UserRouter } from '@routers/user.router'
 
 const app = fastify(FastifyConfig)
 
+const BasicAuthConfig = {
+  validate: async (username: string, password: string) => {
+    const isValid = username === 'jmgr2996' && password === '123456'
+    if (!isValid) return new Error('Invalid username or password')
+  },
+  authenticate: true,
+}
+
 /* Plugins */
 app.register(sensible)
 app.register(swagger)
 app.register(swaggerUI, SwaggerConfig)
 app.register(postgres, PostgresConfig)
+app.register(fastifyAuth)
+app.register(fastifyBasicAuth, BasicAuthConfig)
 
-/* Routers */
+/* Public Routers */
 app.register(HealthRouter)
 app.register(TodosRouter)
 app.register(UserRouter)
+
+/* Private Routers */
+app.after(() => {
+  app.route({
+    method: 'GET',
+    url: '/protected',
+    onRequest: app.auth([app.basicAuth]),
+    handler: async (req, reply) => {
+      return {
+        hello: 'world',
+      }
+    },
+  })
+})
 
 const bootstrap = async (): Promise<void> => {
   try {
